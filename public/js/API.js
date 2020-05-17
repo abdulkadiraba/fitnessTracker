739aff67f82861f87b858fd2209bb6db7ae94702
Abdulkadir Aba <abdulkadir.aba786@gmail.com>
2020-05-17 14:11:26 -0700

    hwFiles

---
+++ public/js/API.js
@@ -0,0 +1,16 @@
+const API = {
+    getLastWorkout: ()=> $.ajax({
+      method: 'GET',
+      url: '/api/workouts'
+    }).then(data=> data.length ? data.slice(-1)[0] : API.createWorkout()),
+    createWorkout: ()=> $.ajax({
+      method: "POST",
+      url: '/api/workouts',
+      data: {}
+    }).then(data=> data),
+    addExercise: (data,id)=> $.ajax({
+      method: "PUT",
+      url: "/api/workouts/"+id,
+      data
+    }).then(data=>data)
+  }
---
+++ public/js/home.js
@@ -0,0 +1,50 @@
+$(document).ready(async ()=>{
+    const lastWorkout = await API.getLastWorkout();
+    console.log(lastWorkout);
+    !lastWorkout.exercises.length ? $("#workoutBtn").text('Start Your Workout') : ''
+    lastWorkout.exercises.map(a=>{
+      $('#exercises').append(`<div class="card" style="width: 18rem;">
+      <div class="card-header">
+        ${a.type}
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">Weight - ${a.weight}</li>
+        <li class="list-group-item">Sets - ${a.sets}</li>
+        <li class="list-group-item">Duration - ${a.duration}</li>
+      </ul>
+    </div>`)
+    })
+  
+    $("#newWorkout").on("click", ()=> API.createWorkout().then(()=>window.location.reload()))
+  
+    $('#selectType').on('change', function(){
+      let type = $(this).val();
+      if(type === "Cardio"){
+        $("#cardio").show();
+        $("#resistance").hide();
+      }else{
+        $("#cardio").hide();
+        $("#resistance").show();
+      }
+    })
+  
+    $("#submitExercise").on("click", function(){
+      let type = $("#selectType").val();
+      let exercise = {};
+      if(type === "Cardio"){
+        exercise.type = "Cardio"
+        exercise.name = $("#cardioName").val().trim();
+        exercise.duration = $("#cardioDuration").val().trim()
+      }else{
+        exercise.type = "Resistance"
+        exercise.name = $("#resistanceName").val().trim();
+        exercise.duration = $("#resistanceDuration").val().trim();
+        exercise.weight= $("#resistanceWeight").val().trim();
+        exercise.repetition = $("#resistanceRep").val().trim();
+        exercise.sets = $("#resistanceSet").val().trim()
+      }
+  
+      API.addExercise(exercise, lastWorkout._id).then(data=> window.location.reload())
+    })
+  
+  })
